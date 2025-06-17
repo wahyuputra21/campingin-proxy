0d9547a63816e2210afba3e098c0847c18021299
wahyuputra21 <ramadhanwahyu534@gmail.com>
2025-06-17 13:05:22 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(req.body),
+      body: JSON.stringify(data),
     });
 
     const contentType = response.headers.get("content-type");
