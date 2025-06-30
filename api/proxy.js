7c14b2bfdc7020f7bf938eaed4d0cbcde0e8c2ab
wahyuputra21 <ramadhanwahyu534@gmail.com>
2025-06-30 19:48:06 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   if (req.method === "OPTIONS") return res.status(200).end();
 
   const MODE = req.query.mode;
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTxHV8SGHZkJFT2MbxKE3K_4Kpr22yZD6eZaUDnXzCdNUgu3PSlQ6SPJ_UY15xImSf6g/exec";
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxdNSkLHBkcEhhLkrqln_Xc0cTvF9M8rreNTzAMXSjyP8Mob2eXRZU7vyBDKC862v2lMg/exec";
 
   // GET: Ambil data stok
   if (req.method === "GET" && MODE === "getstok") {
@@ -112,7 +112,7 @@ if (req.method === "GET" && MODE === "listreparasi") {
 // POST: Barang rusak → kirim ke tab Reparasi
 if (req.method === "POST" && MODE === "reparasi") {
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyTxHV8SGHZkJFT2MbxKE3K_4Kpr22yZD6eZaUDnXzCdNUgu3PSlQ6SPJ_UY15xImSf6g/exec?mode=reparasi", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxdNSkLHBkcEhhLkrqln_Xc0cTvF9M8rreNTzAMXSjyP8Mob2eXRZU7vyBDKC862v2lMg/exec?mode=reparasi", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body)
